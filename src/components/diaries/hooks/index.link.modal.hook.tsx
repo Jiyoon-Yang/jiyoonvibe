f2bf8802d96c393f85838eeed43f2911de5f7f04
Jiyoon-Yang <jiyoon3522@naver.com>
2025-10-16 13:14:10 +0900

    refactor: 일기쓰기 모달 훅 코드 스타일 개선
    - JSDoc 주석 스타일을 프로젝트 표준에 맞게 개선
    - UseDiaryModalReturn 인터페이스 추가로 반환 타입 명시
    - @example 태그 추가하여 사용 예제 제공
    - 각 함수와 속성에 상세한 설명 추가
    - TypeScript 타입 안정성 향상

--- src/components/diaries/hooks/index.link.modal.hook.tsx
+++ src/components/diaries/hooks/index.link.modal.hook.tsx
@@ -2,12 +2,42 @@ import { useModal } from "@/commons/providers/modal/modal.provider";
 import DiariesNew from "@/components/diaries-new";
 
 /**
- * 일기쓰기 모달을 열고 닫는 커스텀 훅
- * @returns {object} openDiaryModal - 일기쓰기 모달을 여는 함수
+ * 일기쓰기 모달 훅의 반환 타입
  */
-export const useDiaryModal = () => {
+export interface UseDiaryModalReturn {
+  /**
+   * 일기쓰기 모달을 여는 함수
+   */
+  openDiaryModal: () => void;
+
+  /**
+   * 모달을 닫는 함수
+   */
+  closeModal: () => void;
+}
+
+/**
+ * 일기쓰기 모달을 열고 닫는 커스텀 훅
+ *
+ * @example
+ * ```tsx
+ * const { openDiaryModal, closeModal } = useDiaryModal();
+ *
+ * // 모달 열기
+ * openDiaryModal();
+ *
+ * // 모달 닫기
+ * closeModal();
+ * ```
+ *
+ * @returns 일기쓰기 모달 제어 함수들
+ */
+export const useDiaryModal = (): UseDiaryModalReturn => {
   const { openModal, closeModal } = useModal();
 
+  /**
+   * 일기쓰기 모달을 엽니다
+   */
   const openDiaryModal = () => {
     openModal(<DiariesNew />);
   };
